5c3c3c8c484125e45c858fbce0c23cc980195695
wxywj0930 <wxyaiwj1314@gmail.com>
2021-01-13 11:14:03 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式文件
+import './style/index.less'
+
 Vue.config.productionTip = false
 
+// 创建Vue根实例
+// 把router配置到根实例中
+// 通过render方法把App根组件渲染到 #app 入口节点
 new Vue({
   router,
   store,
